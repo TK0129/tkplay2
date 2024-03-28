833a001324c1cf9f34ed28fd1353cada67429174
TK0129 <59621736+TK0129@users.noreply.github.com>
2024-03-28 12:04:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ function cleartotal()
    document.getElementById("p1").innerHTML = total;
 }
 
-function adder() {
-  var num1 = parseInt(document.getElementById("myform").elements["num1"].value);
-  var num2 = parseInt(document.getElementById("myform").elements["num2"].value);
-  if (total > 100){
-    console.log("It's over!");
-  } else {
+function adder() 
+{
+   total = 0;
+   var num1 = parseInt(document.getElementById("myform").elements["num1"].value);
+   var num2 = parseInt(document.getElementById("myform").elements["num2"].value);
+  
     total += num1 + num2;
-    document.getElementById("p1").innerHTML = total;
-  }
+   
 }
