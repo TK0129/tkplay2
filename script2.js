907537e75adc7742e7cbfef44627892823698071
TK0129 <59621736+TK0129@users.noreply.github.com>
2024-03-29 10:19:12 -0700

    Create script2.js
    copy of work to date

---
+++ script2.js
@@ -0,0 +1,19 @@
+var total = 0;
+
+function cleartotal()
+{
+   total = 0;
+   document.getElementById("p1").innerHTML = total;
+}
+
+function adder() 
+{
+   if (total > 0) { total = 0; }
+   
+   var num1 = parseInt(document.getElementById("myform").elements["num1"].value);
+   var num2 = parseInt(document.getElementById("myform").elements["num2"].value);
+  
+   total += num1 + num2;
+   document.getElementById("p1").innerHTML = total;
+   
+}
