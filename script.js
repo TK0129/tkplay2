2fe47509c927add190dec7ed4e5e7d105350a98f
TK0129 <59621736+TK0129@users.noreply.github.com>
2024-03-29 16:10:10 -0700

    Update script.js
    correcting syntax

--- script.js
+++ script.js
@@ -17,53 +17,38 @@ var auto51 = 0;
 var auto81 = 0;
 
 
-function cleartotal()
-{
+function cleartotal() {
    total = 0;
-   document.getElementById("p1").innerHTML = total;
+    document.getElementById("p1").innerHTML = total;
 }
 
-function adder() 
-{
+function adder() {
    if (total > 0) { total = 0; }
    
-   var num1 = parseInt(document.getElementById("myform").elements["num1"].value);
-   var num2 = parseInt(document.getElementById("myform").elements["num2"].value);
+    var num1 = parseInt(document.getElementById("myform").elements["num1"].value);
+    var num2 = parseInt(document.getElementById("myform").elements["num2"].value);
   
-   total += num1 + num2;
-   document.getElementById("p1").innerHTML = total;
-   
+    total += num1 + num2;
+    document.getElementById("p1").innerHTML = total;
 }
 
-function clearall()
-{
-   int ar[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }
-   int i, x;
-   for (int i = 1; 1 < 12) {
-   auto[i]=0; 
-   }
-   document.getElementById("p2").innerHTML = auto1;
-}
-
-function autoadd()
-{
-   int i, x;
+function autoadd() {
+   var x;
    x = 0;
-   for (int i = 1 to 11) {
+   for (int i = 0; i < 12; i++ ) {
    x = x + auto[i];
    }
-   document.getElementById("p3").innerHTML = x;
+    document.getElementById("p3").innerHTML = x;
 }
 
 
-function lumpadd()
-{
+function lumpadd() {
    if (total > 0) { total = 0; }
 
-   var num1 = parseInt(document.getElementById("myform").elements["auto1l"].value);
-   var num2 = parseInt(document.getElementById("myform").elements["auto5l"].value);
-   var num3 = parseInt(document.getElementById("myform").elements["auto8l"].value);
+    var num1 = parseInt(document.getElementById("myform").elements["auto1l"].value);
+    var num2 = parseInt(document.getElementById("myform").elements["auto5l"].value);
+    var num3 = parseInt(document.getElementById("myform").elements["auto8l"].value);
 
    total += num1 + num2 + num3;
-   document.getElementById("p4").innerHTML = total;
+    document.getElementById("p4").innerHTML = total;
 }
