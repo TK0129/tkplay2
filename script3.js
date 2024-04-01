e2cf4c2ec2ec80f8c7f20ea72f13ad42d8677b82
TK0129 <59621736+TK0129@users.noreply.github.com>
2024-03-31 19:02:32 -0700

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,10 +1,8 @@
 
 function readmyfile() {
   
-  var fs = require(‘fs’);
-
+  var fs = require('fs');
   var contents = fs.readFileSync(‘C:\Users\tlk01\Documents\TK data\Budget web page doc.LumpSum.txt’, ‘utf8’);
-
   var lines = contents.split(‘/\r?\n/’);
 
   console.log(lines);
