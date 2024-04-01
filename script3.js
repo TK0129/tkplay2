b9d1fe8a2d420aa510bc83b98eeed62190d5203e
TK0129 <59621736+TK0129@users.noreply.github.com>
2024-04-01 13:09:46 -0700

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,12 +1,13 @@
 function readmyfile() {
-
+  
   BufferedReader abc = new BufferedReader(new FileReader(LumpSum.txt));
   List<String> lines = new ArrayList<String>();
   
   while((line = abc.readLine()) != null) {
-      lines.add(line);
-      System.out.println(data);
+    lines.add(line);
+    System.out.println(data);
   }
+  
   abc.close();
   
   // If you want to convert to a String[]
