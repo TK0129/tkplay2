96eb3b847566cc8f7d092f7210b42f2506c9b5a9
TK0129 <59621736+TK0129@users.noreply.github.com>
2024-04-01 11:33:16 -0700

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,8 +1,16 @@
 function readmyfile() {
 
+  
   var fs = require('fs');
   var contents = fs.readFileSync('C:\Users\tlk01\Documents\TK data\Budget web page doc.LumpSum.txt', 'utf8');
   var lines = contents.split('/\r?\n/');
 
   console.log(lines);
+
+      // The lines below solved the issue
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+
 }
